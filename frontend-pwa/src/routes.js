b86e97a8c740c2bfa5a4d53fab449d34b72d5f9c
space_mushroom <matteo.bresciani.997@gmail.com>
2021-02-25 13:48:17 +0000

    auth0 working

--- frontend-pwa/src/routes.js
+++ frontend-pwa/src/routes.js
@@ -21,6 +21,7 @@ import Dashboard from "views/Dashboard.js";
 // import Notifications from "views/Notifications.js";
 import Icons from "views/Icons.js";
 import Bikes from "views/Bikes.js";
+import Login from "views/Login.js"
 // import Typography from "views/Typography.js";
 // import TableList from "views/Tables.js";
 // import Maps from "views/Map.js";
@@ -28,6 +29,13 @@ import Bikes from "views/Bikes.js";
 // import UpgradeToPro from "views/Upgrade.js";
 
 var routes = [
+  {
+    path: "/login",
+    name: "Login",
+    icon: "fas fa-university",
+    component: Login,
+    layout: "/admin",
+  },
   {
     path: "/dashboard",
     name: "Dashboard",
