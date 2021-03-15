24ec5d6289383a7577ae89641d0fca067155ad52
Aishwarya Agarwal <aishwaryaagarwal@Aishwaryas-MacBook-Pro.local>
2021-03-16 03:30:57 +0530

    Add methods to store and fetch  Bus Routes Data

--- sustainableCityManagement/main_project/Bus_API/store_bus_routes_data_in_database.py
+++ sustainableCityManagement/main_project/Bus_API/store_bus_routes_data_in_database.py
@@ -1,26 +1,33 @@
 import sys
 from mongoengine import *
+import mongomock
 import requests
 import json
 import pytz
 import csv
-host_db = "mongodb://127.0.0.1:27017/sustainableCityManagementTest"
-connect("sustainableCityManagementTest", host=host_db)
-
 
 class BusStops(Document):
     stop_name = StringField(max_length=200)
     stop_id = StringField(max_length=200, unique=True)
     stop_lat = DecimalField(precision=3, rounding='ROUND_HALF_UP')
     stop_lon = DecimalField(precision=3, rounding='ROUND_HALF_UP')
-    meta = {'collection': 'Bus_Stops'}
-#     # logger.info('BusUsage document created successfully.')
+    meta = {'collection': 'Bus_Stops'
+    }
+
+class BusRoutes(Document):
+    route_name = StringField(max_length=200)
+    route_id = StringField(max_length=200, unique=True)
+    meta = {'collection': 'Bus_Routes'
+    }
 
 class StoreBusRoutesData:
+    def __init__(self):
+        host_db = "mongodb://127.0.0.1:27017/sustainableCityManagementTest"
+        connect(db = "sustainableCityManagementTest", host=host_db)
+
     def read_bus_stops(self):
         readfile = []
-        with open("../sustainableCityManagement/main_project/Bus_API/resources/stops.csv", "r", encoding="utf8") as f:
-        # with open("./resources/stops.csv", "r", encoding="utf8") as f:
+        with open("./resources/stops.csv", "r", encoding="utf8") as f:
             readfile = list(csv.reader(f))
         return readfile
 
@@ -45,7 +52,35 @@ class StoreBusRoutesData:
             logger.error('Bus Stops data not retrieved from DB')
         return bus_stops
 
+    def read_bus_routes(self):
+        readfile = []
+        # with open("../sustainableCityManagement/main_project/Bus_API/resources/routes.csv", "r", encoding="utf8") as f:
+        with open("./resources/routes.csv", "r", encoding="utf8") as f:
+            readfile = list(csv.reader(f))
+        return readfile
+
+    def store_bus_routes(self):
+        readfile = self.read_bus_routes()
+        for i in range(1,len(readfile)):
+            busroutes = BusRoutes(route_id = readfile[i][0],
+                                route_name = readfile[i][3])
+            try:
+                busroutes.save()
+            except:
+                pass
+
+    def fetch_busroutes(self, locationName = "all"):
+        q_set = BusRoutes.objects()  # Fetch Data from DB
+        # Converts the Processed Bus Data from DB into JSON format
+        json_data = q_set.to_json()
+        bus_routes = json.loads(json_data)
+        if bus_routes is None:
+            logger.error('Bus Routes data is not retrieved from DB')
+        return bus_routes
 
 # a = StoreBusRoutesData()
 # a.store_bus_stops()
 # print(a.fetch_busstops_location()[1])
+
+# a.store_bus_routes()
+# print(a.fetch_busroutes()[1])
