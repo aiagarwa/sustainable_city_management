acd99a8e1c8dffe9708269e98bd956c39128945b
Takshil Bharadwaj <bharadwt@tcd.ie>
2021-03-25 15:35:05 +0000

    Footfall endpoints ready.
    Former-commit-id: 3cdde74538861b739478d8b72571c43343abe71c

--- sustainableCityManagement/main_project/urls.py
+++ sustainableCityManagement/main_project/urls.py
@@ -6,8 +6,10 @@ from .Bus_API.views_bus_api.show_bus_info import BusStopsLocations
 from .Bus_API.views_bus_api.show_bus_info import BusTripsTimings
 from .Bus_API.views_bus_api.show_bus_delays import BusTripDelays
 from .Bus_API.views_bus_api.show_bus_stops import BusStopsLocations
-from .Parkings_API.views_parkings_api.show_parkings_availability import ParkingsAvailability
-from .Parkings_API.views_parkings_api.show_parkings_locations import ParkingsLocations
+# from .Parkings_API.views_parkings_api.show_parkings_availability import ParkingsAvailability
+# from .Parkings_API.views_parkings_api.show_parkings_locations import ParkingsLocations
+from .Footfall_API.views_footfall_api.show_footfall_data import FootfallDatebasedData
+from .Footfall_API.views_footfall_api.show_footfall_data import FootfallOverallData
 
 # Building URL endpoints for API calls.
 urlpatterns = [
@@ -20,6 +22,12 @@ urlpatterns = [
         name='busstops_time_info'),
     url(r'^bustrip_delays/$', BusTripDelays.as_view(),
         name='bustrip_delay_info'),
-    url(r'^parkings_locations/$', ParkingsLocations.as_view(), name ='parkings_locations'),
-    url(r'^parkings_availability/$', ParkingsAvailability.as_view(), name ='parkings_availability'),
+    # url(r'^parkings_locations/$', ParkingsLocations.as_view(),
+    #     name ='parkings_locations'),
+    # url(r'^parkings_availability/$', ParkingsAvailability.as_view(), 
+    #     name ='parkings_availability'),
+    url(r'^footfall_overall/$', FootfallOverallData.as_view(), 
+        name ='footfall_overall_data'),
+    url(r'^footfall_datebased/$', FootfallDatebasedData.as_view(),
+        name ='footfall_datebased_data'),
 ]
