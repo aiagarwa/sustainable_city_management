0e288a3f31c292d63c0555b0cd126cdd6411f75b
Takshil Bharadwaj <bharadwt@tcd.ie>
2021-03-25 14:56:46 +0000

    Added footfall structure.

--- sustainableCityManagement/main_project/Footfall_API/fetch_footfallapi.py
+++ sustainableCityManagement/main_project/Footfall_API/fetch_footfallapi.py
@@ -1,6 +1,6 @@
 import sys
 # from ..Logs.service_logs import bus_log
-from .store_bus_routes_data_in_database import StoreBusRoutesData
+from store_footfall_data_in_database import StoreFootfallData
 # from ..Config.config_handler import read_config
 from datetime import datetime, timedelta
 import copy
@@ -8,19 +8,30 @@ import copy
 # import collections
 import json
 
-class FetchBusApi:
+class FootballApi:
     def __init__(self):
-        self.busRoutesObj = StoreBusRoutesData()
+        self.FootfallObj = StoreFootfallData()
     
-    def bus_stand_locations(self):
+    def footfall_datebased(self, start_date, end_date):
         result_response = {}
-        all_stops = self.busRoutesObj.fetch_busstops_location()
-        counter = 0
-        for location in all_stops:
-            stop_custom_id = "stop_"+str(counter)
-            result_response[stop_custom_id] = {}
-            result_response[stop_custom_id]["STOP_NAME"] = location["stop_name"]
-            result_response[stop_custom_id]["STOP_LAT"] = location["stop_lat"]
-            result_response[stop_custom_id]["STOP_LON"] = location["stop_lon"]
-            counter += 1
+        footfall_dateBased = self.FootfallObj.fetch_data_from_db_for_day(start_date,end_date)
+        for item in footfall_dateBased:
+            location = item["location"]
+            result_response[location] = {}
+            for data in item["footfall_data"]:
+                date = datetime.strftime(data["data_date"],"%Y-%m-%d")
+                result_response[location][date] = data["count"]
         return result_response
+
+    def footfall_overall(self):
+        result_response = {}
+        footfall_overall = self.FootfallObj.fetch_footfall_overall()
+        counter = 0
+        for item in footfall_overall:
+            location = item["location"]
+            result_response[location] = item["count"]
+        return result_response
+
+obj = FootballApi()
+# print(obj.footfall_datebased("2021-01-03","2021-01-05"))
+# print(obj.footfall_overall())
