0dbb585b27b30321608549709e70d16da4509736
space_mushroom <matteo.bresciani.997@gmail.com>
2021-03-26 11:19:40 +0000

    added backend implementation of bus paths
    Former-commit-id: 80faa2f2da05fc53a42d99cbaa646a78b0f76bb8

--- sustainableCityManagement/main_project/urls.py
+++ sustainableCityManagement/main_project/urls.py
@@ -6,6 +6,7 @@ from .Bus_API.views_bus_api.show_bus_info import BusStopsLocations
 from .Bus_API.views_bus_api.show_bus_info import BusTripsTimings
 from .Bus_API.views_bus_api.show_bus_delays import BusTripDelays
 from .Bus_API.views_bus_api.show_bus_stops import BusStopsLocations
+from .Bus_API.views_bus_api.show_bus_paths import BusPathsAPI
 # from .Parkings_API.views_parkings_api.show_parkings_availability import ParkingsAvailability
 # from .Parkings_API.views_parkings_api.show_parkings_locations import ParkingsLocations
 from .Footfall_API.views_footfall_api.show_footfall_data import FootfallDatebasedData
@@ -22,6 +23,8 @@ urlpatterns = [
         name='busstops_time_info'),
     url(r'^bustrip_delays/$', BusTripDelays.as_view(),
         name='bustrip_delay_info'),
+    url(r'^bustrip_paths/$', BusPathsAPI.as_view(),
+        name='bustrip_paths_info'),
     # url(r'^parkings_locations/$', ParkingsLocations.as_view(),
     #     name ='parkings_locations'),
     # url(r'^parkings_availability/$', ParkingsAvailability.as_view(), 
