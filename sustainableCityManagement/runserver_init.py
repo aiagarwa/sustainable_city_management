316be06b30d70e6e7d57a39e65988d809dd74e6d
jmanohar <jmanohar@tcd.ie>
2021-03-26 10:41:03 +0000

    Emergency Service Api
    Former-commit-id: 5f7e55b6e742bd2de47cfbd9725c59cbb49d0cc9

--- sustainableCityManagement/runserver_init.py
+++ sustainableCityManagement/runserver_init.py
@@ -1,20 +1,30 @@
 from main_project.Bike_API.store_bikedata_to_database import StoreBikeDataToDatabase
 from main_project.Bike_API.store_processed_bikedata_to_db import StoreProcessedBikeDataToDB
-from main_project.Logs.service_logs import app_log
+from main_project.Emergency_Service_API.store_emergency_service_data_in_database import StoreServiceData
 from mongoengine import *
+#from main_project.Logs.service_logs import app_log
+# logger = app_log()
+# logger.info('Server_Starts')
 
-logger = app_log()
-logger.info('Server_Starts')
 
 def save_raw_to_database():
     In = input("SAVE RAW DATA IN DB ? :")
     store_bikedata_to_database = StoreBikeDataToDatabase()
+    store_emergency_service_data_to_database = StoreServiceData()
     if In == "yes":
-        store_bikedata_to_database.save_historic_data_in_db(5)
+        store_bikedata_to_database.save_historic_data_in_db(1)
         store_bikedata_to_database.save_bike_stands_location()
+        # store_emergency_service_data_to_database.read_fire_stations()
+        # store_emergency_service_data_to_database.store_fire_stations()
+        # store_emergency_service_data_to_database.read_health_centers()
+        # store_emergency_service_data_to_database.store_health_centers()
+        # store_emergency_service_data_to_database.store_garda_stations()
+        # store_emergency_service_data_to_database.store_garda_stations()
+        # store_emergency_service_data_to_database.read_hospitals()
+        # store_emergency_service_data_to_database.store_hospitals()
     else:
-        logger.error('Storing raw data in DB failed because of key(yes) error')
         pass
+
 
 def save_processed_and_predicted_to_database():
     In = input("SAVE PROCESSED AND PREDICTED DATA IN DB ? :")
@@ -26,19 +36,21 @@ def save_processed_and_predicted_to_database():
     else:
         pass
 
+
 def check_to_drop_database():
     In = input("DROP DATABASE? :")
     store_processed_bike_data_to_db = StoreProcessedBikeDataToDB()
     if In == "yes":
-        conn=connect(host="mongodb://127.0.0.1:27017/sustainableCityManagementTest", alias="default")
+        conn = connect(
+            host="mongodb://127.0.0.1:27017/sustainableCityManagementTest", alias="default")
         conn.drop_database("sustainableCityManagementTest")
-        logger.info('Dropping database')
     else:
         pass
 
 
 def init():
-    connect(host="mongodb://127.0.0.1:27017/sustainableCityManagementTest", alias="default")
+    connect(
+        host="mongodb://127.0.0.1:27017/sustainableCityManagementTest", alias="default")
     check_to_drop_database()
     save_raw_to_database()
     save_processed_and_predicted_to_database()
