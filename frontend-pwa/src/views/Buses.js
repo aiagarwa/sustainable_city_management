5b237cc32bd172a5ea6aeb22525482c09f9bcb6b
space_mushroom <matteo.bresciani.997@gmail.com>
2021-03-26 11:18:39 +0000

    added frontend implementation of trips
    Former-commit-id: 05e024c7b1f0cb514ff0720dad33c728a77c9fad

--- frontend-pwa/src/views/Buses.js
+++ frontend-pwa/src/views/Buses.js
@@ -48,6 +48,24 @@ const iconDefault = L.divIcon({
 
 L.Marker.prototype.options.icon = iconDefault;
 
+function download(data, filename, type) {
+  var file = new Blob([data], {type: type});
+  if (window.navigator.msSaveOrOpenBlob)
+      window.navigator.msSaveOrOpenBlob(file, filename);
+  else { // Others
+      var a = document.createElement("a"),
+              url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = filename;
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);  
+      }, 0);
+  }
+}
+
 class Buses extends React.Component {
   setBusesGraph(x, y) {
     console.log(x, y);
@@ -80,7 +98,7 @@ class Buses extends React.Component {
     });
   }
 
-  drawPathBetweenBusStops(startStop, destinationStop) {
+  drawPathBetweenBusStopsIdSaveToFile(startStop, destinationStop) {
     // const start = [53.344, -6.233];
     // const destination = [53.342, -6.236];
 
@@ -90,9 +108,9 @@ class Buses extends React.Component {
     let start = null;
     let destination = null;
     for (const busStop of this.state.busStops) {
-      if (busStop.stop == startStop) {
+      if (busStop.stop_id == startStop) {
         start = [busStop.lat, busStop.lng];
-      } else if (busStop.stop == destinationStop) {
+      } else if (busStop.stop_id == destinationStop) {
         destination = [busStop.lat, busStop.lng];
       }
 
@@ -104,6 +122,15 @@ class Buses extends React.Component {
     axios
       .get(`https://api.openrouteservice.org/v2/directions/driving-hgv?api_key=${apiKey}&start=${start.reverse().join(',')}&end=${destination.reverse().join(',')}`)
       .then(res => {
+        let start_stop_coordinates = {
+          "start": startStop,
+          "end": destinationStop,
+          coordinates: res.data.features[0].geometry.coordinates
+        }
+
+        this.state.save_trips.paths.push(start_stop_coordinates)
+        this.setState({ save_trips: this.state.save_trips });
+
         let latlngs = [];
         latlngs.push(start.reverse());
         for (const c of res.data.features[0].geometry.coordinates) {
@@ -133,10 +160,14 @@ class Buses extends React.Component {
       });
   }
 
-  drawPathBetweenBusStopsId(startStop, destinationStop) {
-    // const start = [53.344, -6.233];
-    // const destination = [53.342, -6.236];
+  getCoordinatesFromStartStop(startStop, destinationStop) {
+    for(let i=0; i<this.state.bus_paths.length; i++) {
+      if(this.state.bus_paths[i].start_stop_id == startStop && this.state.bus_paths[i].end_stop_id == destinationStop)
+        return this.state.bus_paths[i].coordinates
+    }
+  }
 
+  drawPathBetweenBusStopsId(startStop, destinationStop) {
     const map = this.state.map;
 
     // Retrieve start & destination stops coordinates
@@ -152,37 +183,63 @@ class Buses extends React.Component {
       if (start && destination) break;
     }
 
-    const apiKey = '5b3ce3597851110001cf62489c45fd4df8464534ba7a6bab835d5cc8';
+    let coordinates = this.getCoordinatesFromStartStop(startStop, destinationStop)
 
+    let latlngs = [];
+    latlngs.push(start);
+    for (const c of coordinates) {
+      latlngs.push([c.lat, c.lon]);
+    }
+    latlngs.push(destination);
+
+    console.log(latlngs)
+
+    const options = {
+      "delay": 400,
+      "dashArray": [
+        10,
+        20
+      ],
+      "weight": 5,
+      "color": "#0000FF",
+      "pulseColor": "#FFFFFF",
+      "paused": false,
+      "reverse": false,
+      "hardwareAccelerated": true
+    };
+
+    const antPolyline = new AntPath(latlngs, options);
+    antPolyline.addTo(map);
+  }
+
+  populateBusStopsSaveToFile() {
     axios
-      .get(`https://api.openrouteservice.org/v2/directions/driving-hgv?api_key=${apiKey}&start=${start.reverse().join(',')}&end=${destination.reverse().join(',')}`)
+      .get('http://127.0.0.1:8000/main/busstop_locations/')
       .then(res => {
-        let latlngs = [];
-        latlngs.push(start.reverse());
-        for (const c of res.data.features[0].geometry.coordinates) {
-          latlngs.push(c.reverse());
+        let busStops_tmp = [];
+        let busStops_list = [];
+        let counter = 0;
+        for (const stop of Object.keys(res.data.DATA.RESULT)) {
+          //if (counter++ > 800) break;
+          if (busStops_tmp.includes(res.data.DATA.RESULT[stop].STOP_ID) == false) {
+            busStops_tmp.push(res.data.DATA.RESULT[stop].STOP_ID);
+            busStops_list.push({
+              stop: stop,
+              stop_id: res.data.DATA.RESULT[stop].STOP_ID,
+              name: res.data.DATA.RESULT[stop].STOP_NAME,
+              lat: res.data.DATA.RESULT[stop].STOP_LAT,
+              lng: res.data.DATA.RESULT[stop].STOP_LON,
+              icon: iconDefault,
+              });
+           }
         }
-        latlngs.push(destination.reverse());
+        this.setState({ busStops: busStops_list });
 
-        const options = {
-          "delay": 400,
-          "dashArray": [
-            10,
-            20
-          ],
-          "weight": 5,
-          "color": "#0000FF",
-          "pulseColor": "#FFFFFF",
-          "paused": false,
-          "reverse": false,
-          "hardwareAccelerated": true
-        };
-
-        const antPolyline = new AntPath(latlngs, options);
-        antPolyline.addTo(map);
+        // this.drawPathBetweenBusStops("stop_76", "stop_85");
+        this.populateTripsSaveToFile();
       })
-      .catch(err => {
-        console.log(err);
+      .catch(error => {
+        console.error(error);
       });
   }
 
@@ -209,7 +266,6 @@ class Buses extends React.Component {
         }
         this.setState({ busStops: busStops_list });
 
-        // this.drawPathBetweenBusStops("stop_76", "stop_85");
         this.populateTrips();
       })
       .catch(error => {
@@ -217,17 +273,85 @@ class Buses extends React.Component {
       });
   }
 
+  drawPathForTripSaveToFile(index) {
+    //console.log(this.state.busTrips)
+    let trip = this.state.busTrips[index];
+    let stops_list = trip.stops_list;
+    let context = this;
+
+    for(let i=0; i<stops_list.length-1; i++) {
+      setTimeout(function() {
+        //console.log(stops_list[i].stop_id)
+        //console.log(stops_list[i+1].stop_id)
+        context.drawPathBetweenBusStopsIdSaveToFile(stops_list[i].stop_id, stops_list[i+1].stop_id);
+        //console.log(i);
+      }, i * 2_000);
+    }
+
+    //download(JSON.stringify(this.state.save_trips), "trips_paths", "txt")
+  }
+
   drawPathForTrip(index) {
-    console.log(this.state.busTrips)
+    //console.log(this.state.busTrips)
     let trip = this.state.busTrips[index];
     let stops_list = trip.stops_list;
 
     for(let i=0; i<stops_list.length-1; i++) {
-      console.log(stops_list[i].stop_id)
-      console.log(stops_list[i+1].stop_id)
       this.drawPathBetweenBusStopsId(stops_list[i].stop_id, stops_list[i+1].stop_id);
     }
 
+    //download(JSON.stringify(this.state.save_trips), "trips_paths", "txt")
+  }
+
+  populateTripsSaveToFile(){
+    axios
+      .get('http://127.0.0.1:8000/main/busstop_timings/')
+      .then(res => {
+        let trips_list = [];
+        for (const trip of Object.keys(res.data.DATA.RESULT)) {
+          trips_list.push({
+            trip: trip,
+            trip_id: res.data.DATA.RESULT[trip].TRIP_ID,
+            route_id: res.data.DATA.RESULT[trip].ROUTE_ID,
+            stops_list: res.data.DATA.RESULT[trip].STOP_INFO,
+            icon: iconDefault,
+            });
+        }
+        
+        let context = this;
+        this.setState({ busTrips: trips_list });
+
+        let max_val = 0
+        for(let i=0; i<this.state.busTrips.length; i++) {
+          if(this.state.busTrips[i].stops_list.length > max_val)
+            max_val = this.state.busTrips[i].stops_list.length
+        }
+
+        for(let i=0; i<this.state.busTrips.length; i++) {
+          setTimeout(function() {
+            context.drawPathForTripSaveToFile(i);
+          }, i * 90_000);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+  drawTrips() {
+    axios
+      .get('http://127.0.0.1:8000/main/bustrip_paths/')
+      .then(res => {
+        let result = res.data.DATA.RESULT;
+        this.setState({ bus_paths: result });
+
+        for(let i=0; i<this.state.busTrips.length; i++) {
+          this.drawPathForTrip(i);
+        }
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   populateTrips(){
@@ -246,13 +370,20 @@ class Buses extends React.Component {
         }
         
         this.setState({ busTrips: trips_list });
-        for(let i=0; i<this.state.busTrips.length; i++) {
-          this.drawPathForTrip(i);
-        }
+        this.drawTrips();
       })
       .catch(error => {
         console.error(error);
       });
+  }
+
+  componentDidMountSaveToFile() {
+    this.populateBusStopsSaveToFile();
+    let context = this;
+
+    setTimeout(function() {
+      download(JSON.stringify(context.state.save_trips), "trips_paths", "txt")
+    }, 90_000 * 130);
   }
 
   componentDidMount() {
@@ -275,10 +406,12 @@ class Buses extends React.Component {
       series: [],
       busSelection: "ALL",
       graphLoading: true,
+      save_trips: {"paths": []},
+      bus_paths: {}
     };
 
     this.mapCreated = this.mapCreated.bind(this);
-    this.drawPathBetweenBusStops = this.drawPathBetweenBusStops.bind(this);
+    //this.drawPathBetweenBusStops = this.drawPathBetweenBusStops.bind(this);
   }
 
   onChangeBusSelection(e) {
@@ -319,7 +452,7 @@ class Buses extends React.Component {
                         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                       />
-                      {this.state.busStops.map(
+                      {/* {this.state.busStops.map(
                         ({ stop, name, lat, lng, icon }, idx) => (
                           <Marker
                             key={`marker-${idx}`}
@@ -329,12 +462,12 @@ class Buses extends React.Component {
                             <Popup>
                               <p>
                                 <b>{name}</b><br />
-                                {/* <i>{stop}</i> */}
+                                {/* <i>{stop}</i> //////////////////////////////////////////////////////*}
                               </p>
                             </Popup>
                           </Marker>
                         )
-                      )}
+                      )} */}
                     </MapContainer>
                   </div>
                 </CardBody>
