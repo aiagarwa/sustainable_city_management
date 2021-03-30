113caab53310b2130fe83faa847758226304cb3e
space_mushroom <matteo.bresciani.997@gmail.com>
2021-03-30 10:13:54 +0100

    added frontend for parking
    Former-commit-id: 03c4ca0890c927a0588ab21c78e61575e8f81574

--- frontend-pwa/src/views/Parkings.js
+++ frontend-pwa/src/views/Parkings.js
@@ -49,99 +49,49 @@ const iconDefault = L.divIcon({
 L.Marker.prototype.options.icon = iconDefault;
 
 class Parkings extends React.Component {
-  getParkingCoordinates() {
-    try {
-      // const res = await axios.get(
-      //   "http://127.0.0.1:8000/main/bikestands_details/?type=live"
-      // );
-      // const bikeStationsLive = res.data.DATA.RESULT;
-      // return bikeStationsLive;
-      let parkings_coordinates = [
-        {
-          name: "PARNELL",
-          area: "Northwest",
-          lat: 53.35081,
-          lng: -6.26831,
-        },
-        {
-          name: "ILAC",
-          area: "Northwest",
-          lat: 53.34861,
-          lng: -6.26884,
-        },
-        {
-          name: "JERVIS",
-          area: "Northwest",
-          lat: 53.34878,
-          lng: -6.26667,
-        },
-        {
-          name: "ARNOTTS",
-          area: "Northwest",
-          lat: 53.34908,
-          lng: -6.26006,
-        },
-        {
-          name: "MARLBORO",
-          area: "Northeast",
-          lat: 53.352600098081375,
-          lng: -6.258366086014968,
-        },
-        {
-          name: "ABBEY",
-          area: "Northeast",
-          lat: 53.35024325062157,
-          lng: -6.254233328658077,
-        },
-        {
-          name: "THOMASST",
-          area: "Southwest",
-          lat: 53.34381779026483,
-          lng: -6.2802188590302395,
-        },
-        {
-          name: "C/CHURCH",
-          area: "Southwest",
-          lat: 53.3433754859322,
-          lng: -6.269683162040512,
-        },
-        {
-          name: "SETANTA",
-          area: "Southeast",
-          lat: 53.342046862254755,
-          lng: -6.256021086015311,
-        },
-        {
-          name: "DAWSON",
-          area: "Southeast",
-          lat: 53.340471400850085,
-          lng: -6.256018776761035,
-        },
-        {
-          name: "TRINITY",
-          area: "Southeast",
-          lat: 53.34416060960417,
-          lng: -6.262745776634299,
-        },
-        {
-          name: "GREENRCS",
-          area: "Southeast",
-          lat: 53.342438538176374,
-          lng: -6.263818974717609,
-        },
-        {
-          name: "DRURY",
-          area: "Southeast",
-          lat: 53.342987236356,
-          lng: -6.2631027690892935,
-        },
-        {
-          name: "B/THOMAS",
-          area: "Southeast",
-          lat: 53.34268459927202,
-          lng: -6.261434643686948,
-        },
-      ];
+  setParkingMarkers(parkings_coordinates_dictionary) {
+    let { markers } = this.state;
+
+    axios.get("http://127.0.0.1:8000/main/parkings_availability/").then(
+      async (res) => {      
+      let parkings_avaliabilities = res.data.DATA.RESULT[0].parkings;
+      console.log(parkings_avaliabilities)
+
+      for (let i = 0; i < parkings_avaliabilities.length; i++) {
+        let parking_name = parkings_avaliabilities[i].name;
+
+        // Add markers
+        markers.push({
+          name: parking_name,
+          area: parkings_coordinates_dictionary[parking_name].area,
+          position: [
+            parkings_coordinates_dictionary[parking_name].lat,
+            parkings_coordinates_dictionary[parking_name].lng,
+          ],
+          availableSpaces: parkings_avaliabilities[i].availableSpaces,
+          icon: {
+            className: "custom-pin",
+            iconAnchor: [0, 24],
+            labelAnchor: [-6, 0],
+            popupAnchor: [0, -36],
+            html: `<i class="fa fa-map-marker-alt fa-3x" style="color:red;"></i>`,
+          },
+      });
+    }
+
+    console.log(markers);
+    this.setState({ markers });
+
+
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+  getParkingCoordinatesAndSetMarkers() {
+    axios.get("http://127.0.0.1:8000/main/parkings_locations/").then(
+      async (res) => {
+      const parkings_coordinates = res.data.DATA.RESULT;
 
       let parkings_coordinates_dictionary = {};
 
@@ -153,74 +103,14 @@ class Parkings extends React.Component {
         };
       }
 
-      return parkings_coordinates_dictionary;
-    } catch (e) {
-      console.log(e);
-    }
+      this.setParkingMarkers(parkings_coordinates_dictionary)
+    }).catch((err) => {
+      console.log(err);
+    });
   }
 
   componentDidMount() {
-    // axios
-    //   .get("http://127.0.0.1:8000/main/bikestands_details/?type=locations")
-    //   .then(async (res) => {
-
-    let { markers } = this.state;
-
-    // const bikeStations = res.data.DATA.RESULT;
-    const result = {
-      updateTimestamp: "2021-03-22 10:12:53",
-      parkings: [
-        {
-          name: "PARNELL",
-          area: "Northwest",
-          availableSpaces: 338,
-        },
-        {
-          name: "ILAC",
-          area: "Northwest",
-          availableSpaces: 12,
-        },
-        {
-          name: "THOMASST",
-          area: "Southwest",
-          availableSpaces: 256,
-        },
-        {
-          name: "DAWSON",
-          area: "Southeast",
-          availableSpaces: null, // May happen if no recent data from the API; just put "No data" in the front-end display?
-        },
-      ],
-    };
-
-    const parkings_avaliabilities = result.parkings;
-    const parkings_coordinates = this.getParkingCoordinates();
-    console.log(parkings_coordinates["PARNELL"]);
-
-    for (let i = 0; i < parkings_avaliabilities.length; i++) {
-      let parking_name = parkings_avaliabilities[i].name;
-
-      // Add markers
-      markers.push({
-        name: parking_name,
-        area: parkings_coordinates[parking_name].area,
-        position: [
-          parkings_coordinates[parking_name].lat,
-          parkings_coordinates[parking_name].lng,
-        ],
-        availableSpaces: parkings_avaliabilities[i].availableSpaces,
-        icon: {
-          className: "custom-pin",
-          iconAnchor: [0, 24],
-          labelAnchor: [-6, 0],
-          popupAnchor: [0, -36],
-          html: `<i class="fa fa-map-marker-alt fa-3x" style="color:red;"></i>`,
-        },
-      });
-    }
-
-    console.log(markers);
-    this.setState({ markers });
+    this.getParkingCoordinatesAndSetMarkers();
     // localStorage.setItem("parkings_availability", JSON.stringify(markers));
     // })
     // .catch((err) => {
